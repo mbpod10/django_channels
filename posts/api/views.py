a7d56a86722614c4b6fabf281abc5c612e36cd53
M. Brock Podgurski <mbpod10@gmail.com>
2023-03-28 09:39:12 -0400

    change url patterns

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import Post
-from .api.serializers import PostSerializer
+from ..models import Post
+from .serializers import PostSerializer
 from rest_framework.decorators import action
 
 class PostViewsets(viewsets.ModelViewSet):
